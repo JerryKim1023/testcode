10b361ce9d2f3e1a5549d78c3178bad838718a6e
JerryKim <ecec1023@gmail.com>
2022-05-12 18:42:07 +0900

    django-stub,mypy set up done

--- sparta/settings.py
+++ sparta/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from typing import List
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-de0!ky#%2%fojg7^kclfwo6o10sqb)mahywud)8$uu%u+nqsd6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
